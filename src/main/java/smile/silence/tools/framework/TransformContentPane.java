b4fbc3513d53a98c8406886a15cd4e29df19932d
hzxiexin <hzxiexin@corp.netease.com>
2016-09-24 15:04:14 +0800

    增加语法高亮

--- src/main/java/smile/silence/tools/framework/TransformContentPane.java
+++ src/main/java/smile/silence/tools/framework/TransformContentPane.java
@@ -1,5 +1,8 @@
 package smile.silence.tools.framework;
 
+import org.fife.ui.rsyntaxtextarea.RSyntaxTextArea;
+import org.fife.ui.rsyntaxtextarea.SyntaxConstants;
+
 import java.awt.BorderLayout;
 import java.awt.Font;
 import javax.swing.BorderFactory;
@@ -11,7 +14,7 @@ public class TransformContentPane
   extends JPanel
 {
   private static TransformContentPane ourInstance = new TransformContentPane();
-  private final JTextArea textArea = new JTextArea();
+  private final RSyntaxTextArea textArea = new RSyntaxTextArea();
   
   public static TransformContentPane getInstance()
   {
@@ -22,8 +25,9 @@ public class TransformContentPane
   {
     super(new BorderLayout());
     setBorder(BorderFactory.createEmptyBorder(10, 10, 10, 10));
-    this.textArea.setLineWrap(true);
-    this.textArea.setFont(new Font("������������", 0, 14));
+    textArea.setLineWrap(true);
+    textArea.setFont(new Font("微软雅黑", 0, 14));
+    textArea.setSyntaxEditingStyle(SyntaxConstants.SYNTAX_STYLE_XML);
     add(new JScrollPane(this.textArea));
   }
   
